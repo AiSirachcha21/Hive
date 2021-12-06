3f76526a5bd165eb5f777eba592b1c20932a3223
Ryan Kuruppu <ryan@ascentic.se>
2021-12-07 02:41:06 +0530

    Added Static Routes class for easy access to page routes

---
+++ Hive/Client/Shared/Constants/Routes.cs
@@ -0,0 +1,9 @@
+﻿namespace Hive.Client.Shared.Constants
+{
+    public static class Routes
+    {
+        public const string Index = "/";
+        public const string Login = "/login";
+        public const string Register = "/register";
+    }
+}
