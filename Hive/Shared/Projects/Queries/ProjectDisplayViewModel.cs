6d5013c8ad127e4838d5e9523a403c9f4923e0d1
Ryan Kuruppu <ryan@ascentic.se>
2021-12-11 15:36:50 +0530

    Added Missing ViewModel used for Project GetByUser query

---
+++ Hive/Shared/Projects/Queries/ProjectDisplayViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hive.Shared.Projects.Queries
+{
+    public class ProjectDisplayViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
