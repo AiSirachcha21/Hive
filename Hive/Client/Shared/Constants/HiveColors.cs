0db6e2722ea237e928e6b4c160f2c914e11a4d00
Ryan Kuruppu <ryan@ascentic.se>
2021-12-12 22:19:41 +0530

    Added Class with const strings for colors

---
+++ Hive/Client/Shared/Constants/HiveColors.cs
@@ -0,0 +1,11 @@
+﻿namespace Hive.Client.Shared.Constants
+{
+    public static class HiveColors
+    {
+        public const string Orange = "#f2994a";
+        public const string OrangeDark = "#da8a43";
+        public const string OrangeLighten = "#f3a35c";
+        public const string White = "#fff";
+        public const string Grey = "#333";
+    }
+}
