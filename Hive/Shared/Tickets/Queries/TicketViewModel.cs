3ebfda2390acc7f4ddd3d6b2ea6103b26f40682b
Ryan Kuruppu <ryan@ascentic.se>
2021-12-21 05:35:55 +0530

    Added ViewModels for TicketUser and Ticket

---
+++ Hive/Shared/Tickets/Queries/TicketViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Hive.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.Shared.Tickets.Queries
+{
+    public class TicketViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AssignedUserId { get; set; }
+        public string AssignedUserName { get; set; }
+        public TicketStatus TicketStatus { get; set; }
+        public string LastUpdated { get; set; }
+    }
+}
