25d1f8ba07f56e740cccaa45a380664d913c6a97
Ryan Kuruppu <ryan@ascentic.se>
2021-12-23 19:38:31 +0530

    Added Command to Remove role from a user

---
+++ Hive/Server/Application/Users/Commands/RemoveRoleFromUser/RemoveRoleFromUserCommand.cs
@@ -0,0 +1,31 @@
+﻿using Hive.Domain;
+using Hive.Server.Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hive.Server.Application.Users.Commands.RemoveRoleFromUser
+{
+    public record RemoveRoleFromUserCommand(string UserId, string Role) : IRequest<bool>;
+
+    public class RemoveRoleFromUserCommandHandler : IRequestHandler<RemoveRoleFromUserCommand, bool>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RemoveRoleFromUserCommandHandler(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<bool> Handle(RemoveRoleFromUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+            var result = await _userManager.RemoveFromRoleAsync(user, request.Role);
+            await _context.SaveChangesAsync();
+
+            return result.Succeeded;
+        }
+    }
+}
---
+++ Hive/Server/Application/Users/Commands/RemoveRoleFromUser/RemoveRoleFromUserCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Hive.Server.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hive.Server.Application.Users.Commands.RemoveRoleFromUser
+{
+    public class RemoveRoleFromUserCommandValidator : AbstractValidator<RemoveRoleFromUserCommand>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RemoveRoleFromUserCommandValidator(ApplicationDbContext context)
+        {
+            _context = context;
+
+            RuleFor(c => c)
+                .MustAsync(BeAUserRole).WithMessage("User with Role already exists");
+        }
+
+        private async Task<bool> BeAUserRole(RemoveRoleFromUserCommand command, CancellationToken cancellationToken)
+        {
+            var commandRoleId = (await _context.Roles.FirstAsync(r => r.Name == command.Role)).Id;
+            return await _context.UserRoles.AnyAsync(ur => ur.UserId == command.UserId & ur.RoleId == commandRoleId);
+        }
+    }
+}
