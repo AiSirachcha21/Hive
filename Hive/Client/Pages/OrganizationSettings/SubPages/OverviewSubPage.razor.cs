4c50e278c9c87c717379d5355c9719d158c6ead8
Ryan Kuruppu <ryan@ascentic.se>
2022-01-06 02:34:54 +0530

    Added OnDelete call to organization settings page

--- Hive/Client/Pages/OrganizationSettings/SubPages/OverviewSubPage.razor.cs
+++ Hive/Client/Pages/OrganizationSettings/SubPages/OverviewSubPage.razor.cs
@@ -5,6 +5,13 @@ using Hive.Client.Services.Organizations;
 using Hive.Shared.Organizations.QueryViewModels;
 using Fluxor;
 using Hive.Client.Shared.Store.OrganizationSettings;
+using Hive.Client.Components.ParamConstants;
+using Hive.Client.Components.RequireConfirmationDialog;
+using Hive.Client.Shared.Store.Organizations.Actions;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+using System.Linq;
+using Hive.Client.Shared.Constants;
 
 namespace Hive.Client.Pages.OrganizationSettings.SubPages
 {
@@ -13,6 +20,9 @@ namespace Hive.Client.Pages.OrganizationSettings.SubPages
         [Inject] IOrganizationService OrganizationService { get; set; }
         [Inject] IState<OrganizationSettingsState> OrganizationSettingsState { get; set; }
         [Inject] IDispatcher Dispatcher { get; set; }
+        [Inject] IDialogService DialogService { get; set; }
+        [Inject] NavigationManager Navigator { get; set; }
+        [CascadingParameter] Task<AuthenticationState> AuthenticationState { get; set; }
         [CascadingParameter] Guid OrganizationId { get; set; }
 
         string _organizationName;
@@ -62,5 +72,24 @@ namespace Hive.Client.Pages.OrganizationSettings.SubPages
         bool IsNameEdited() => _organizationName != OrganizationSettingsVm.Name;
         bool IsNameFieldEmpty => string.IsNullOrEmpty(_organizationName);
         bool IsSavable() => IsNameEdited() && _isUniqueOrgName && !string.IsNullOrEmpty(_organizationName);
+
+        async void OnDelete(string name, Guid id)
+        {
+            string username = new((await AuthenticationState).User.Identity.Name.Where(s => !char.IsWhiteSpace(s)).ToArray());
+            DialogParameters dialogParams = new()
+            {
+                { RequireConfirmationDialogParams.ConfirmButtonText, "Delete" },
+                { RequireConfirmationDialogParams.DialogContent, $"You are about to delete {name}. This change is irreversable, are you sure you wish to continue ?" },
+                { RequireConfirmationDialogParams.IsDelete, true },
+                { RequireConfirmationDialogParams.ConfirmationPhrase, $"{username}/{name}" }
+            };
+            IDialogReference dialog = DialogService.Show<RequireConfirmationDialog>("Are you sure?", dialogParams);
+            DialogResult dialogResult = await dialog.Result;
+            if (!dialogResult.Cancelled && (bool)dialogResult.Data)
+            {
+                Dispatcher.Dispatch(new DeleteOrganizationAction(id));
+                Navigator.NavigateTo(Routes.UserOrganizations);
+            }
+        }
     }
 }
