d9369e9274117ef7d93fd4abe4efdff57efff3fb
Ryan Kuruppu <ryan@ascentic.se>
2022-01-06 13:27:32 +0530

    Created new models for Creating and Updating Ticket request

---
+++ Hive/Shared/Tickets/Commands/CreateTicketRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hive.Shared.Tickets.Commands
+{
+    public class CreateTicketRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AssigneeUserId { get; set; }
+        public Guid ProjectId { get; set; }
+    }
+}
---
+++ Hive/Shared/Tickets/Commands/UpdateTicketRequest.cs
@@ -0,0 +1,18 @@
+﻿using Hive.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.Shared.Tickets.Commands
+{
+    public class UpdateTicketRequest
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AssignedUserId { get; set; }
+        public TicketStatus Status { get; set; }
+    }
+}
